2b6f0e03ba6bea33df6df429fb0855f6ca2d5b80
Jason Johnson <145226961+JJohnson606@users.noreply.github.com>
2024-02-28 22:35:12 +0000

    added_features

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,17 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show, :edit, :update, :destroy]
+  before_action :foo, only: [:new, :index]
+  def foo
+    p "hiya" * 100
+  end
+
   def new
+    self.foo
     @movie = Movie.new
   end
 
   def index
+    self.foo
     @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
@@ -14,12 +22,11 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    movie_params = params.require(:movie).permit(:title, :description)
-    
+    #movie_params = params.require(:movie).permit(:title, :description, :image_url)
+
     @movie = Movie.new(movie_params)
 
     if @movie.valid?
@@ -32,14 +39,13 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params.fetch(:id))
   end
 
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    movie_params = params.require(:movie).permit(:title, :description)
-    
+    #movie_params = params.require(:movie).permit(:title, :description, :image_url)
+
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
@@ -54,4 +60,15 @@ class MoviesController < ApplicationController
 
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :image_url, :released_on)
+  end
+
+  def set_movie
+    @movie = Movie.find(params.fetch(:id))
+  end
+
 end
